b2ff708737c63f3c25bbf01c5cc3bb44280aa9c9
C. M. de Picciotto <d3p1@d3p1.dev>
2025-10-30 20:48:20 +0000

    chore: adjust app base path [#1]

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type {NextConfig} from 'next'
 
 const nextConfig: NextConfig = {
-  output: 'export'
-};
+  output: 'export',
+  basePath: '/nextjs-torus',
+}
 
-export default nextConfig;
+export default nextConfig
