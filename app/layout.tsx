84b27160014922ca6cc17b2f43cce250f8e96c12
C. M. de Picciotto <d3p1@d3p1.dev>
2025-11-01 13:25:35 +0000

    fix: solve error useSearchParams() should be wrapped in a suspense boundary [#1]

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@
  */
 import type {Metadata} from 'next'
 import './globals.css'
+import {Suspense} from 'react'
 
 export const metadata: Metadata = {
   title: 'Next.js | Torus',
@@ -17,7 +18,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className="w-screen h-screen bg-black">{children}</body>
+      <body className="w-screen h-screen bg-black text-white">
+        <Suspense>{children}</Suspense>
+      </body>
     </html>
   )
 }
---
+++ app/loading.tsx
@@ -0,0 +1,7 @@
+/**
+ * @description Loader
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export default function Loading() {
+  return <p>Loading...</p>
+}
